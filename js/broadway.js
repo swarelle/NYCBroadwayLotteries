60301992dfa9e9db32b25fc055b7fa62089e91c1
Ellen Fu <efu16@bloomberg.net>
2018-06-08 10:21:36 -0400

    Refactor per-page code into fillForm.txt Local JS code cannot be executed after a page has already loaded.

--- js/broadway.js
+++ js/broadway.js
@@ -1,5 +1,5 @@
 const directEnter = [
-		"http://aladdinthemusical.com/lottery", // Aladdin
+		"https://lottery.broadwaydirect.com/show/aladdin/", // Aladdin
 		"https://lottery.broadwaydirect.com/show/angels/", // Angels in America
 		"https://lottery.broadwaydirect.com/show/sponge/", // SpongeBob SquarePants
 		"https://lottery.broadwaydirect.com/show/wicked/" // Wicked
@@ -21,35 +21,6 @@ const bulkEnter = [
 		"http://www.luckyseat.com/springsteen-broadway/" // Springsteen on Broadway
 	];
 
-let directory,
-    firstName,
-    lastName,
-    email,
-    zipCode,
-    phoneNumber,
-    birthDay,
-    birthMonth,
-    birthYear;
-
-/*
- * Prompts user for information used to fill out forms
- */
-function setupInfo() {
-  // alert("Please fill out your personal information. This will only be used to" +
-  //  " enter the lottery and will not be saved anywhere.");
-  directory = prompt("Full directory of the bfbb folder");
-}
-
-/*
- * Runs the program
- */
-function run() {
-  setupInfo();
-	enterDirectLotteries();
-	enterSocialLotteries();
-	enterBulkLotteries();
-}
-
 /*
  * Opens each url in urls in a new window and return all the opened window objects.
  *
@@ -64,27 +35,13 @@ function openPages(urls) {
   return opened;
 }
 
-function appendScript(opened, script) {
-	for (const win of opened) {
-		const scriptEl = win.document.createElement("script");
-    scriptEl.src = script;
-    win.document.head.appendChild(scriptEl);
-	}
-}
-
-function enterDirectLotteries() {
-  const opened = openPages(directEnter);
-  appendScript(opened, directory + "/js/directLottery.js");
-}
-
-function enterSocialLotteries() {
-  const opened = openPages(socialEnter);
-  appendScript(opened, directory + "/js/socialLottery.js");
-}
-
-function enterBulkLotteries() {
-  const opened = openPages(bulkEnter);
-  appendScript(opened, directory + "/js/bulkLottery.js");
+/*
+ * Runs the program
+ */
+function run() {
+  openPages(bulkEnter);
+  openPages(directEnter);
+  openPages(socialEnter);
 }
 
 run();
